1e0876480137bb931cfacd430984aef0850deeb3
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2023-12-01 15:18:48 +0300

    AngularNgıf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:`
+  <h1>Şart Blokları</h1>
+<button *ngIf="!isActive; else check" (click)="changeActiveStatus">
+Aktif et
+</button>
+<br><br>
+<ng-template #check>
+<button  (click)="changeActiveStatus()">
+Pasife Al
+</button>
+</ng-template>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'conditenal';
+  isActive:boolean=false;
+  changeActiveStatus(){
+    this.isActive=this.isActive;
+  }
 }
